8e0456f648e849271b982f9b2db63adb80bafda5
fede369 <37138663+fede369@users.noreply.github.com>
2018-11-27 16:58:20 +0100

    1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,36 @@
-function preload(){
-  // put preload code here
+var canvas;
+var myMap;
+var myLoc;
+var w = 800;
+var h = 700;
+var key =
+  "pk.eyJ1IjoibWFwcGF1c2VyIiwiYSI6ImNqNXNrbXIyZDE2a2cyd3J4Ym53YWxieXgifQ.JENDJqKE1SLISxL3Q_T22w";
+
+var options = {
+  lat: 0,
+  lng: 0,
+  zoom: 12,
+  style: "mapbox://styles/mapbox/dark-v9",
+  pitch: 10
+};
+
+var mappa = new Mappa("MapboxGL", key);
+
+function preload() {
+  myLoc = getCurrentPosition();
 }
 
 function setup() {
-  // put setup code here
+  canvas = createCanvas(w, h).parent("canvasHolder");
+  options.lat = myLoc.latitude;
+  options.lng = myLoc.longitude;
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
 }
 
 function draw() {
-  // put drawing code here
+  clear();
+  var point = myMap.latLngToPixel(myLoc.latitude, myLoc.longitude);
+  fill("orange");
+  ellipse(point.x, point.y, 10);
 }
